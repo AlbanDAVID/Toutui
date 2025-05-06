d66df0bc93c35134bdfdf4cf5c5ea85b036742d8
AlbanDAVID <alban.dav@live.fr>
2025-05-06 15:16:53 +0200

    wip: install bin

--- hello_toutui.sh
+++ hello_toutui.sh
@@ -221,6 +221,7 @@ source_cargo_env() {
     if [[ $SHELL =~ \/(sh|bash|zsh|ash|pdksh) ]]; then
         if [[ -z "${CARGO_HOME}" ]]; then
             source "$HOME/.cargo/env"
+            echo" DONE SOURCE"
         else
             source "${CARGO_HOME}/env"
         fi
