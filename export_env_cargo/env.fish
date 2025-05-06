c805fbc83c376e15f1310190c97cc4fa6b7de958
AlbanDAVID <alban.dav@live.fr>
2025-05-06 16:36:19 +0200

    Update env.fish

--- export_env_cargo/env.fish
+++ export_env_cargo/env.fish
@@ -1,5 +1,4 @@
-# rustup shell setup
 if not contains "$HOME/.cargo/bin" $PATH
-    # Prepending path in case a system-installed rustc needs to be overridden
+    # Prepending path in case a system-installed binary needs to be overridden
     set -x PATH "$HOME/.cargo/bin" $PATH
 end
