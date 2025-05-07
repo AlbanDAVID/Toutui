0210276490d5f628b6e7b57bd40c0fc88368ca27
AlbanDAVID <alban.dav@live.fr>
2025-05-07 22:17:45 +0200

    wip: install process

--- src/utils/clap.rs
+++ src/utils/clap.rs
@@ -1,7 +1,42 @@
-use clap::Command;
+use clap::{Arg, Command};
 
 pub fn clap() {
-    let _matches = Command::new("toutui")
-        .version(env!("CARGO_PKG_VERSION")) 
+    let matches = Command::new("toutui")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author("Alban DAVID")
+        .about("TUI audiobookshelf client")
+        .arg(
+            Arg::new("update")
+                .long("update")
+                .help("Run update script via curl")
+                .action(clap::ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("uninstall")
+                .long("uninstall")
+                .help("Run uninstall script via curl")
+                .action(clap::ArgAction::SetTrue),
+        )
         .get_matches();
+
+    if matches.get_flag("uninstall") {
+        std::process::Command::new("sh")
+            .arg("-c")
+            .arg(
+                r#"bash -c 'tmpfile=$(mktemp) && curl -LsSf https://github.com/AlbanDAVID/Toutui/raw/install_with_cargo/hello_toutui.sh -o "$tmpfile" && bash "$tmpfile" uninstall && rm -f "$tmpfile"'"#,
+            )
+            .status()
+            .expect("failed to run uninstall script");
+    }
+    if matches.get_flag("update") {
+        std::process::Command::new("sh")
+            .arg("-c")
+            .arg(
+                r#"bash -c 'tmpfile=$(mktemp) && curl -LsSf https://github.com/AlbanDAVID/Toutui/raw/install_with_cargo/hello_toutui.sh -o "$tmpfile" && bash "$tmpfile" update && rm -f "$tmpfile"'"#,
+            )
+            .status()
+            .expect("failed to run uninstall script");
+    }
+
 }
+
